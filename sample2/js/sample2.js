dbecc8e5956ad9e601ad9c22baa9f446df555308
hiroghap1 <hiro_ghap1@yahoo.co.jp>
2021-03-14 18:38:18 +0900

    sample2修正

--- sample2/js/sample2.js
+++ sample2/js/sample2.js
@@ -1,14 +1,25 @@
 const hamburgerBtn = document.getElementById('hamburger-btn');
 const body = document.querySelector('body');
 const overlay = document.createElement('div');
+const mediaQueryList = matchMedia('(min-width: 768px)');
 overlay.classList.add('overlay');
 body.appendChild(overlay);
 
 hamburgerBtn.addEventListener('click',activeToggle,false);
 overlay.addEventListener('click',activeToggle,false);
+onMediaChange(mediaQueryList);
+mediaQueryList.addEventListener('change',onMediaChange);
 
 function activeToggle() {
     hamburgerBtn.classList.toggle('active');
     overlay.classList.toggle('active');
     body.classList.toggle('fixed');
+}
+
+function onMediaChange(mq) {
+    if (mq.matches) {
+        hamburgerBtn.classList.remove('active');
+        body.classList.remove('fixed');
+        overlay.classList.remove('active');
+    }
 }
