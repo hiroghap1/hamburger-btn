60677fb66ab6b3c5f40460480e0ef713079ad10f
hiroghap1 <hiro_ghap1@yahoo.co.jp>
2021-07-10 00:37:51 +0900

    画像ファイル追加

--- sample1/js/sample1.js
+++ sample1/js/sample1.js
@@ -2,3 +2,22 @@ const hamburgerBtn = document.getElementById('hamburger-btn');
 hamburgerBtn.addEventListener('click',function(){
     this.classList.toggle('active');
 });
+//
+// const options = {
+//     root: null, // 今回はビューポートをルート要素とする
+//     rootMargin: "0px 0px", // ビューポートの中心を判定基準にする
+//     threshold: 0 // 閾値は0
+// };
+//
+// const observer = new IntersectionObserver(function (entries) {
+//     for( let i = 0; i < entries.length; i++) {
+//         if(entries[i].isIntersecting) {
+//             entries[i].target.classList.add('show');
+//         }
+//     }
+// },options);
+//
+// const elm = document.querySelectorAll('p');
+// for( let i = 0; i < elm.length; i++) {
+//     observer.observe(elm[i]);
+// }
