97c6a36c9a447d09d9d262ce3666fbdbd973f9fd
hiroghap1 <hiro_ghap1@yahoo.co.jp>
2024-07-21 21:51:05 +0900

    feat(sample2):HTML変更

--- sample1/js/sample1.js
+++ sample1/js/sample1.js
@@ -3,6 +3,8 @@ hamburgerBtn.addEventListener('click',()=>{
     hamburgerBtn.classList.toggle('active');
 });
 
+
+/* 以下はスクロールアニメーションのサンプル */
 const options = {
     root: null, // 今回はビューポートをルート要素とする
     rootMargin: "0px 0px", // ビューポートの中心を判定基準にする
@@ -21,3 +23,20 @@ const elm = document.querySelectorAll('p');
 elm.forEach((el)=> {
     observer.observe(el);
 });
+
+
+performance.mark('myPerformanceStart') // 開始点
+console.log(document.querySelector('#hamburger-btn')); // 計測する処理
+performance.mark('myPerformanceEnd') // 終了点
+
+performance.measure(
+    'myPerformance', // 計測名
+    'myPerformanceStart', // 計測開始点
+    'myPerformanceEnd' // 計測終了点
+);
+
+// 結果の取得
+const results = performance.getEntriesByName('myPerformance');
+
+// 表示
+console.log(results[0]);
